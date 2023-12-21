2b4b48333945aef51030b553918f5fce943e096c
Bartłomiej Kochanek <bkochanek4@gmail.com>
2023-12-21 22:26:42 +0100

    Create utility for bencoding data

---
+++ dht_peer_finder/bencode/__init__.py
@@ -0,0 +1,75 @@
+def encode(data):
+    if isinstance(data, str):
+        data = data.encode()
+
+    if isinstance(data, int):
+        return b"i" + str(data).encode("ascii") + b"e"
+
+    elif isinstance(data, bytes):
+        return str(len(data)).encode("ascii") + b":" + data
+
+    elif isinstance(data, list):
+        return b"l" + b"".join(encode(item) for item in data) + b"e"
+
+    elif isinstance(data, dict):
+        return (
+            b"d"
+            + b"".join(encode(key) + encode(data[key]) for key in sorted(data))
+            + b"e"
+        )
+
+    else:
+        raise TypeError(f"Value: {data!r} is of invalid type: {type(data).__name__}")
+
+
+def decode(data):
+    def parse_int(data):
+        end_index = data.index(b"e")
+        return int(data[1:end_index]), end_index + 1
+
+    def parse_bytes(data):
+        colon_index = data.index(b":")
+        length = int(data[:colon_index])
+        start_index = colon_index + 1
+        end_index = start_index + length
+        return data[start_index:end_index], end_index
+
+    def parse_list(data):
+        result = []
+        index = 1
+        while data[index] != ord("e"):
+            item, index = parse_item(data[index:])
+            result.append(item)
+        return result, index + 1
+
+    def parse_dict(data):
+        result = {}
+        index = 1
+        while data[index] != ord("e"):
+            key, index_offset = parse_bytes(data[index:])
+            index += index_offset
+            value, index_offset = parse_item(data[index:])
+            index += index_offset
+            result[key] = value
+        return result, index + 1
+
+    def parse_item(data):
+        first_char = data[0]
+
+        if first_char == ord("i"):
+            return parse_int(data)
+
+        elif first_char == ord("l"):
+            return parse_list(data)
+
+        elif first_char == ord("d"):
+            return parse_dict(data)
+
+        elif first_char >= ord("0") and first_char <= ord("9"):
+            return parse_bytes(data)
+
+        else:
+            raise ValueError(f"Invalid bencoded data: {data!r}")
+
+    result, _ = parse_item(data)
+    return result
